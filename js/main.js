1023acd1e4d94bfccb9f0d2efaf6eef74972733d
Mike <michael.reynolds@goziohealth.com>
2023-12-28 10:56:01 -0600

    Removed old files

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// Setup footer text
-const node = document.getElementById("footer");
-const footerText = document.createElement("p");
-footerText.textContent = "Gozio Links";
-node.appendChild(footerText);
